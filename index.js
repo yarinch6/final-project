a4b5cf45e872a36292e42d851170fa0eae18297c
yarin cohen <yarinch6@gmail.com>
2022-06-07 18:20:12 +0300

    add mongodb connection

--- index.js
+++ index.js
@@ -3,11 +3,34 @@ const res = require('express/lib/response')
 const app = express()
 const port = 3000
 
+const mydb = require("./models/dbAdapterConnect")
+
 app.use(express.static('public'))
 
 
-app.get('/saveStudent', (req, res) => {
-  res.send(req.query)
+app.get('/getResult', (req, res) => {
+
+  let fname = "Shiraz"
+
+  async function myProgram(fname) {
+    await mydb.findName(fname).then((result)=>res.send(result))
+  }
+  myProgram(fname)
+
+})
+
+app.get('/getCustomer', (req, res) => {
+  var newCustomer = {
+    name:req.query.fname,
+    email:req.query.email,
+    phoneNumber:req.query.phone 
+  }
+
+  async function mysave(customerDetails) {
+    await mydb.saveCustomer(customerDetails).then((result) => res.send(result));
+  }
+
+  mysave(newCustomer);
 })
 
 
@@ -16,7 +39,3 @@ app.listen(port, () => {
 })
 
 
-// async function mysave(details) {
-//   await mydb.savePerson(details).then((result) => res.send(result));
-// }
-// mysave(newOrder);
--- models/dbAdapterConnect.js
+++ models/dbAdapterConnect.js
@@ -1,1 +1,39 @@
-const {MongoClient} = require("mongodb")
+const {MongoClient} = require("mongodb");
+const cli = require("nodemon/lib/cli");
+
+const uri = "mongodb://localhost:27017/";
+const Client = new MongoClient(uri);
+
+async function findMongoDoc (fname) {
+
+    //connect
+    await Client.connect();
+    console.log("connect succes");
+
+    //check
+    await Client.db("admin").command({ping : 1 })
+
+    //find one
+    const myResult = await Client.db("SukenikHairDesign")
+    .collection("customers")
+    .findOne({name : `${fname}` });
+
+
+    //close
+    await Client.close();
+
+    return myResult;
+
+}
+
+async function saveCustomer(customerDetails) {
+    var Client = new MongoClient(uri);
+    await Client.connect();
+    var col = Client.db("SukenikHairDesign").collection("customers");
+    var result = await col.insertOne(customerDetails);
+    Client.close();
+    return result;
+}
+
+exports.findName = findMongoDoc;
+exports.saveCustomer = saveCustomer;
